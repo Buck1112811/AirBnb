f8861155ef0f8e3b8a3257b9b69ea5a6da6f1467
buck <jachnickidamianjan@gmail.com>
2025-10-20 00:07:20 +0900

    feat init navbar

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import Navbar from "./components/navbar/Navbar";
+import "./globals.css";
+
+const font = Nunito({
+  subsets: ["latin"]
+})
+
+export const metadata: Metadata = {
+  title: "Airbnb",
+  description: "Airbnb clone",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={font.className}
+      >
+        <Navbar /> 
+        {children}
+      </body>
+    </html>
+  );
+}
